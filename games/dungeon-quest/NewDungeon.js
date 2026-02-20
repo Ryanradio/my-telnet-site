e5f777af70acaaf825ded533293c91326090a1ae
Ryanradio <96878996+Ryanradio@users.noreply.github.com>
2026-02-20 17:42:19 -0500

    Dungeon Editor update — 2/20/2026 5:35:48 PM

---
+++ games/dungeon-quest/NewDungeon.js
@@ -0,0 +1,11 @@
+window.DUNGEONS = window.DUNGEONS || {};
+window.DUNGEONS.NewDungeon = {
+  "name": "New Dungeon",
+  "description": "",
+  "floors": {
+    "1": {
+      "startRoom": null,
+      "rooms": {}
+    }
+  }
+};
